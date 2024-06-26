443a36034623e2836fbfc75be24bb8e473422636
alhassanyoussef <hyoussef650@.com>
2024-06-26 22:01:45 +0300

    airbnb

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This defines a class Userr"""
+"""This module defines a class User"""
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -8,7 +8,7 @@ from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """This defines a user byy various attributes"""
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
